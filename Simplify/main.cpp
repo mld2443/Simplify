1738279bdcde2d7b482e1ee2571f1fb684f02015
Matthew Dillard <7395051@gmail.com>
2015-11-28 20:32:46 -0600

    Initial Commit

---
+++ Simplify/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simplify
+//
+//  Created by Matthew Dillard on 11/28/15.
+//  Copyright © 2015 Matthew Dillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
